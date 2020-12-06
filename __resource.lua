ef756a737323510fb9860ff962dd5742c3baac75
unknown <_             __ _>
2020-12-06 13:19:48 -0500

    VinaFramework update

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,13 @@
 game 'gta5'
-client_script 'VinaFrameworkClient.dll'
 client_script 'FiveM_Voice.net.dll'
 server_script 'FiveM_VoiceServer.net.dll'
+
+ui_page 'assets/index.html'
+
+files {
+	"Newtonsoft.Json.dll",
+	"VinaFrameworkClient.dll",
+	"assets/index.html",
+	"assets/scripts/main.js",
+	"assets/styles/main.css",
+}
